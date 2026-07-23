f32c2023f90f4de58028163694c7f628285560ba
Kyler Fredericks <a02312825@usu.edu>
2026-07-22 23:57:46 -0600

    Added pointer that follows stock line, flickers a bit though

--- scenes/minigames/StocksGame/StocksGame.cs
+++ scenes/minigames/StocksGame/StocksGame.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public partial class StocksGame : Node
 {
@@ -10,6 +11,8 @@ public partial class StocksGame : Node
 	public Node2D? Player;
 	[Export]
 	public Camera2D? Camera;
+	[Export]
+	public Sprite2D? Marker;
 
 	const int START_POINTS = 100;
 	const int POINT_GAP = 20;
@@ -34,17 +37,12 @@ public partial class StocksGame : Node
 		noise.Frequency = 0.002f;
 		noise.FractalOctaves = 4;
 
-		for (lastPointX = 0; lastPointX < START_POINTS * POINT_GAP; lastPointX += POINT_GAP)
+		for (lastPointX = -100; lastPointX <= START_POINTS * POINT_GAP; lastPointX += POINT_GAP)
 		{
 			int noiseValue = (int) (Mathf.Tanh(noise.GetNoise2D(lastPointX - 600, 0) * 3.0f) * 300.0f);
 			lastPointY = noiseValue + GD.RandRange(-30, 30) + 324;
-			
-			// if (lastPointY > MAX_NUM) { lastPointY = MAX_NUM; }
-			// if (lastPointY < MIN_NUM) { lastPointY = MIN_NUM; }
 
 			points.Enqueue(new Vector2(lastPointX, lastPointY));
-
-			GD.Print("First point: " + lastPointX);
 		}
 
 		Line.Points = points.ToArray();
@@ -55,26 +53,33 @@ public partial class StocksGame : Node
 		ArgumentNullException.ThrowIfNull(this.Line);
 		ArgumentNullException.ThrowIfNull(this.Player);
 		ArgumentNullException.ThrowIfNull(this.Camera);
+		ArgumentNullException.ThrowIfNull(this.Marker);
 
 		Player.Position = new Vector2(Player.Position.X + 1, Player.Position.Y);
 		Camera.Position = new Vector2(Player.Position.X - 500, 324);
 
-	if (Player.Position.X > lastPointX - 600)
+	if (Player.Position.X > points.First().X + 1100)
 		{
 			int noiseValue = (int) (Mathf.Tanh(noise.GetNoise2D(lastPointX - 600, 0) * 3.0f) * 300.0f);
 			lastPointY = noiseValue + GD.RandRange(-30, 30) + 324;
-			// if (lastPointY > MAX_NUM) { lastPointY = MAX_NUM; }
-			// if (lastPointY < MIN_NUM) { lastPointY = MIN_NUM; }
 			points.Enqueue(new Vector2(lastPointX, lastPointY));
 			lastPointX += POINT_GAP;
 
 			points.Dequeue();
+		}
+		else
+		{
+			float yValue1 = points.ElementAt(54).Y;
+			float yValue2 = points.ElementAt(55).Y;
 
-			GD.Print("First point in process: " + lastPointX);
+
+			float index = Player.Position.X % 20;
+			float yFinal = yValue1 - ((yValue1 - yValue2) * (index / 20));
+
+			// GD.Print("Left Y Value: " + yValue1 + "\tRight Y Value: " + yValue2 + "\tIndex: " + index + "\tFinal Y Value: " + yFinal);
+			Marker.Position = new Vector2(0, yFinal);
 		}
 
 		Line.Points = points.ToArray();
 	}
-
-
 }
